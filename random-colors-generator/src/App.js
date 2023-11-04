b83848a672e984b17be760e777f302aaf074021b
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-04 01:45:34 +0000

    upload project `random-colors-generator` with react

---
+++ random-colors-generator/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './components/cards';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World, React!</h1>
+      <ul>
+        <li>Start project Random Colors Generate</li>
+      </ul>
+      <Card title="Cartao 1" content="forma correta" />
+    </div>
+  );
+}
+
+export default App;
